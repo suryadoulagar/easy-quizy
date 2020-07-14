5523582c93812845bbd81288790a8757d5be4787
suryadoulagar <suryadoulagar@gmail.com>
2020-07-14 21:17:53 +0530

    all files committed

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+const correctAnswers = ["A", "B", "C", "D"];
+const form = document.querySelector(".quizy");
+const result = document.querySelector(".result");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score += 25;
+        }
+    });
+    
+    scrollTo(0,0);
+    result.classList.remove("d-none");
+
+    // output animation
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector("span").textContent = `${output}%`
+        if(output === score){
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
+});
